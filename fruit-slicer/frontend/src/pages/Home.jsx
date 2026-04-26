7b2c871221b172447a8da20142c801515ed5a1ba
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-04-26 08:17:30 +0545

    image route changed

---
+++ fruit-slicer/frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
+
+const Home = ()=>{
+    const navigate = useNavigate();
+
+    const floatingFruits = [
+    {
+        img
+    }
+  ];
+
+    return(
+        <>
+
+        </>
+    )
+}
+export default Home;
